9382452d8bb9aba7f5889de025f40685e0715044
dataweston <98360199+dataweston@users.noreply.github.com>
2025-07-22 11:19:19 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDRRxTT1ZNsRFMRWqRO4u2Czh0xUmS2SLI",
+    authDomain: "budget-calendar-1ded1.firebaseapp.com",
+    projectId: "budget-calendar-1ded1",
+    storageBucket: "budget-calendar-1ded1.firebasestorage.app",
+    messagingSenderId: "1065927239680",
+    appId: "1:1065927239680:web:0b4058ceb15168b1c5d0e5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
